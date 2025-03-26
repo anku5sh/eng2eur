23ab90abd2fdc07047a28e37e5153977d9179a4e
anku5sh <ankush4play@yahoo.co.in>
2025-03-26 14:53:40 +0100

    fix: code cleanup

--- fix_paths.py
+++ fix_paths.py
@@ -1,6 +1,4 @@
-import sys
-import os
-
-if getattr(sys, 'frozen', False):
-    os.environ['QT_MAC_WANTS_LAYER'] = '1'
-    os.environ['RESOURCES_DIR'] = sys._MEIPASS
+import sys, os
+if getattr(sys, "frozen", False):
+    os.environ["QT_MAC_WANTS_LAYER"] = "1"
+    os.environ["RESOURCES_DIR"] = sys._MEIPASS
